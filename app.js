aae0f4bd676b9f68b68f9e5ce85258c95c0118ae
Vedovelli <vedovelli@gmail.com>
2015-09-14 06:58:38 -0300

    Aula 14

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ new Vue({
     el: '#beerApp',
 
     data: {
+        select2: null,
+        visibleColumns: ['name', 'last_mod'],
+        columnsToFilter: [],
+        filterTerm: '',
+        all: [],
         cervejarias: [],
         openDetails: [],
         sortColumn: 'name',
@@ -20,6 +25,40 @@ new Vue({
     },
 
     methods: {
+
+        doResetAll: function()
+        {
+            var self = this;
+
+            self.$set('visibleColumns', ['name', 'last_mod']);
+            self.$set('columnsToFilter', []);
+            self.$set('filterTerm', '');
+            self.$set('cervejarias', self.all);
+            self.$set('openDetails', []);
+            self.$set('sortColumn', 'name');
+            self.$set('sortInverse', false);
+
+            self.select2.val('').trigger('change');
+        },
+
+        doFilter: function()
+        {
+            var self = this,
+            filtered = self.all;
+
+            if(self.filterTerm != '' && self.columnsToFilter.length > 0)
+            {
+                filtered = _.filter(self.all, function(cervejaria)
+                {
+                    return self.columnsToFilter.some(function(column)
+                    {
+                        return cervejaria[column].toLowerCase().indexOf(self.filterTerm.toLowerCase()) > -1
+                    });
+                });
+            }
+
+            self.$set('cervejarias', filtered);
+        },
 
         doSort: function(ev, column)
         {
@@ -70,6 +109,14 @@ new Vue({
         self.$http.get('http://api.beer.app/cervejarias', function(response)
         {
             self.$set('cervejarias', response);
+            self.$set('all', response);
+        });
+
+        self.select2 = jQuery(self.$$.columnsToFilterSelect).select2({
+            placeholder: 'Selecionar uma ou mais colunas para filtrar!'
+        }).on('change', function()
+        {
+            self.$set('columnsToFilter', jQuery(this).val());
         });
     }
 });
